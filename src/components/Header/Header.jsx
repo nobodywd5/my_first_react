be07f60e2398cb8c72a5ebc3d8ea907cd2588f83
nobodywd5 <kosskill2011@gmail.com>
2019-07-06 18:05:59 +0200

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                alt="start-my"
+                src="https://domenafirmy.pl/wp-content/uploads/2018/09/domenafirmy-logo.svg"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://domenafirmy.pl/wp-content/uploads/2018/09/miastodf.jpg' alt="img"/>
+            </div>
+            <div>
+                ava + my description
+                <img
+                    src='https://domenafirmy.pl/wp-content/uploads/2018/09/animacja-768x576.jpg' alt="img"/>
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile;
